28669ab35459aad57fb7e89b3f5b5af6af7a5a3e
Lola Rufino <lola.ruf.ar@gmail.com>
2021-10-14 15:54:46 +0200

    Add animations to words in About component

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,20 +4,23 @@
       <article class="article-wrapper">
         <h2 class="article-title">Who am I?</h2>
         <p class="article-text">
-          I went from photographer to web developer, deciding to change camera's
-          "click" for the mouse's one.
+          I went from <span class="anim-text-flow">photographer</span> to
+          <span class="anim-text-flow">web developer</span>, deciding to change
+          camera's "click" for the mouse's one.
         </p>
         <p class="article-text">
-          The first time I wrote my first console.log() I felt the same
-          butterflies in my belly than the first time I developed my first film
-          roll. So in 2020 I moved out of my natal Tenerife to Barcelona. And in
-          2021, I focused my professional career on Web Development, a passion I
-          always had.
+          The first time I wrote my first
+          <span class="anim-text-flow">console.log()</span> I felt the same
+          <span class="anim-text-flow">butterflies in my belly</span> than the
+          first time I developed my first film roll. So in 2020 I moved out of
+          my natal Tenerife to Barcelona. And in 2021, I focused my professional
+          career on web development, a passion I always had.
         </p>
         <p class="article-text">
-          Trained at Skylab Coders Academy (now ISDI Coders), world's best
-          bootcamp of 2021 (according to SwitchUp), I keep learning about all
-          the new technologies I find challenging.
+          Trained at Skylab Coders Academy (now ISDI Coders),
+          <span class="anim-text-flow">world's best bootcamp</span> of 2021
+          (according to SwitchUp), I keep learning about all the new
+          technologies I find challenging.
         </p>
       </article>
       <article class="article-wrapper">
@@ -45,15 +48,17 @@
       <article class="article-wrapper">
         <h2 class="article-title">Skills</h2>
         <p class="article-text">
-          Normally, for frontend I like to use Typescript and Javascript for
-          backend. I really like to alternate between the languages. For the
-          frontend area I know how to work with HTML, CSS, SASS and BEM. I have
-          developed with Vue using Vuex and React using Redux to apply Flux
-          pattern. For the backend area I know how to work with NodeJS,
-          ExpressJS, MongoDB, Mongoose, PostMan, Passport, JWT, API Restful. I
-          always test my code with unit test using Jest. Other technologies:
-          Git, GitHub, SonarQube, EsLint, Trello, Slack, Excalidraw, Figma,
-          Notion.
+          Normally, for frontend I like to use
+          <span class="anim-text-flow">Typescript</span> and
+          <span class="anim-text-flow">Javascript</span> for backend. I really
+          like to alternate between the languages. For the frontend area I know
+          how to work with HTML, CSS, SASS and BEM. I have developed with Vue
+          using Vuex and React using Redux to apply
+          <span class="anim-text-flow">Flux pattern</span>. For the backend area
+          I know how to work with NodeJS, ExpressJS, MongoDB, Mongoose, PostMan,
+          Passport, JWT, API Restful. I always test my code with unit test using
+          Jest. Other technologies: Git, GitHub, SonarQube, EsLint, Trello,
+          Slack, Excalidraw, Figma, Notion.
         </p>
         <img
           class="tea-gif"
@@ -102,6 +107,7 @@
         color: $gray;
         text-shadow: 3px 3px 2px $green;
         text-decoration: none;
+        font-weight: 900;
         &:hover {
           text-shadow: 0px 0px 0px;
         }
@@ -153,6 +159,38 @@
         width: 40%;
       }
     }
+  }
+}
+#{$animationElement} {
+  animation-name: anim-text-flow-keys;
+  animation-duration: #{$animationDuration}s;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+  animation-fill-mode: forwards;
+  text-shadow: 1px 1px $gray;
+  border-radius: 10px 0 10px 0;
+  font-weight: 900;
+}
+
+/*
+   * Keyframe loop
+   */
+@keyframes anim-text-flow-keys {
+  @for $i from 0 through $animationSteps {
+    #{percentage($i * (1 / $animationSteps))} {
+      color: hsla(random(365), 60, 60, 1);
+    }
+  }
+}
+
+/*
+   * Element animation delay loop
+   */
+$totalDelayTime: $animationElementsCount * $delayBetweenLetters;
+
+@for $i from 1 through $animationElementsCount {
+  #{$animationElement}:nth-of-type(#{$i}) {
+    animation-delay: #{($i * $delayBetweenLetters) - $totalDelayTime}s;
   }
 }
 </style>
